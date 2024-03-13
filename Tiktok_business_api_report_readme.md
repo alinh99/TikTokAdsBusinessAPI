85fc701ab825f40d067ee0ab35700058f7d53c62
alinh99 <alinh1803@gmail.com>
2024-03-13 22:33:37 +0700

    first commit

---
+++ Tiktok_business_api_report_readme.md
@@ -0,0 +1,3 @@
+Run tiktok_business_api_report.py file
+To get access token:
+Read How to get Access Token.docx
---
+++ constants.py
@@ -0,0 +1,2 @@
+URL_ADVERTISER_IDS = 'https://business-api.tiktok.com/open_api/v1.3/oauth2/advertiser/get/'
+URL_ADVERTISER_INFO = 'https://business-api.tiktok.com/open_api/v1.3/advertiser/info/'
